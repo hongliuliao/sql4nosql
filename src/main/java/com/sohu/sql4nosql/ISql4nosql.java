e45c1a3dfd9235cc0ff7a819c3eb9acba1579e7c
hongliuliao <hongliuliao@sohu-inc.com>
2012-07-05 18:35:31 +0800

    add ISql4nosql interface

---
+++ src/main/java/com/sohu/sql4nosql/ISql4nosql.java
@@ -0,0 +1,22 @@
+/**
+ * $Id$
+ */
+package com.sohu.sql4nosql;
+
+import java.util.List;
+
+/**
+ * Query the records by sql
+ * @author hongliuliao
+ *
+ * CreateDate:2012-7-5 обнГ06:17:15
+ */
+public interface ISql4nosql {
+	/**
+	 * Query the records which match with sql
+	 * @param sql the sql for query records
+	 * @param params the params for sql
+	 * @return the list which query by sql
+	 */
+	public List<?> queryForList(String sql,Object...params);
+}
