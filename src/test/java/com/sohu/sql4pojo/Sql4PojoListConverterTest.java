2cc3c0824b6fc67097373294a26021735505ef48
hongliuliao <hongliuliao@sohu-inc.com>
2012-07-05 20:12:02 +0800

    add ISql4nosql interface

--- src/test/java/com/sohu/sql4pojo/Sql4PojoListConverterTest.java
+++ src/test/java/com/sohu/sql4pojo/Sql4PojoListConverterTest.java
@@ -1,7 +1,6 @@
 package com.sohu.sql4pojo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -14,22 +13,19 @@ public class Sql4PojoListConverterTest {
 	private Sql4PojoListConverter converter;
 	@Before
 	public void init() {
-		users = Arrays.asList(new MockUser(1,"tom",new Date()),new MockUser(2,"jerry",new Date()));
+		users.add(new MockUser(1,"tom",new Date()));
+		users.add(new MockUser(2,"jerry",new Date()));
+		users.add(new MockUser(3,"jim",new Date()));
 		converter = new Sql4PojoListConverter(users);
 	}
 	@Test
-	public void testQueryFromPojoList_selectAll() throws Exception {
-		List<Map<String, ?>> result = converter.queryForList("select * from users");
-		System.out.println("testQueryFromPojoList_selectAll:"+result);
-	} 
-	@Test
-	public void testQueryFromPojoList_selectOne() throws Exception {
+	public void testQueryForList_selectOne() throws Exception {
 		List<Map<String, ?>> result = converter.queryForList("select name from users");
-		System.out.println("testQueryFromPojoList_selectOne:"+result);
+		System.out.println("testQueryForList_selectOne:"+result);
 	} 
 	@Test
-	public void testQueryFromPojoList_selectSome() throws Exception {
+	public void testQueryForList_selectSome() throws Exception {
 		List<Map<String, ?>> result = converter.queryForList("select id,name from users where name='tom'");
-		System.out.println("testQueryFromPojoList_selectSome:" + result);
+		System.out.println("testQueryForList_selectSome:" + result);
 	} 
 }
