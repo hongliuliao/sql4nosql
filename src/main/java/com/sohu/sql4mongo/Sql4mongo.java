4b2027534bfa8d2e20f470468ffb8708e217c5f4
hongliuliao <hongliuliao@sohu-inc.com>
2012-07-19 09:57:47 +0800

    add sql4mongo

---
+++ src/main/java/com/sohu/sql4mongo/Sql4mongo.java
@@ -0,0 +1,52 @@
+/**
+ * $Id$
+ */
+package com.sohu.sql4mongo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.sohu.sql4nosql.ISql4nosql;
+import com.sohu.sql4nosql.QuerySqlStruct;
+import com.sohu.sql4nosql.utils.AntlrUtils;
+
+/**
+ * 
+ * @author hongliuliao
+ *
+ * CreateDate:2012-7-18 ÏÂÎç08:23:59
+ */
+public class Sql4mongo implements ISql4nosql {
+	private DB db;
+	
+	public Sql4mongo(String host, int port, String dbName, String collectionName) {
+		try {
+			Mongo mongo = new Mongo(host, port);
+			db = mongo.getDB(dbName);
+		} catch (Exception e) {
+			throw new RuntimeException("Can not create mongo instance!",e);
+		}
+	}
+	
+	public List<?> queryForList(String sql, Object... params) {
+		QuerySqlStruct sqlStruct = AntlrUtils.buildSqlStruct(sql);
+		String collectionName = sqlStruct.getTableName();
+		DBCollection collection = db.getCollection(collectionName);
+		BasicDBObject query = new BasicDBObject();
+		query.put(sqlStruct.getWhereFieldName(), sqlStruct.getFieldValue());
+		DBCursor cursor = collection.find(query);
+		List<DBObject> dbObjects = new ArrayList<DBObject>();
+		while(cursor.hasNext()) {
+			DBObject dbObject = cursor.next();
+			dbObjects.add(dbObject);
+		}
+		return dbObjects;
+	}
+
+}
